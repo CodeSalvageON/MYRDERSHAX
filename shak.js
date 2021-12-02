5667b1fb96aa5fc40af012dbe2c98451802f1d0b
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-12-02 10:21:54 -0500

    Update shak.js

--- shak.js
+++ shak.js
@@ -1,4 +1,41 @@
-function encryptBase (key, base) {
-  let string_key = String(key);
+function encryptBase (base, key) {
+  let preset_key = key;
+  let preset_base = base;
+  
+  if (key === null || key === undefined || key === "") {
+    preset_key = "";
+  }
+  
+  if (base === null || base === undefined || base === "") {
+    preset_base = "";
+  }
+  
+  let string_key = String(preset_key);
   let string_base = String(base);
+  
+  let compressed_base = LZString.compress(string_base);
+  let compressed_key = LZString.compress(string_key);
+  
+  let encrypted_base = "";
+  
+  for (i=0; i < encrypted_base.length; i++) {
+    encrypted_base += encrypted_base[i] + compressed_key;
+  }
+  
+  return encrypted_base;
+}
+
+function decryptBase (base, key) {
+  let preset_key = key;
+  let preset_base = base;
+  
+  if (key === null || key === undefined || key === "") {
+    preset_key = "";
+  }
+  
+  if (base === null || base === undefined || base === "") {
+    preset_base = "";
+  }
+  
+  
 }
